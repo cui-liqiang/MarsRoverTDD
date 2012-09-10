0cb86f5bc6a43168768986661fb205c666a2131e
Cui Liqiang <cui.liqiang@gmail.com>
2012-09-11 00:53:36 +0800

    Revert "delete a lot of tests"
    This reverts commit 217cc41420473a6ec35ba48176b17a95a503689b.

---
+++ src/test/java/tw/wn/rover/RoverTest.java
@@ -0,0 +1,30 @@
+package tw.wn.rover;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import tw.wn.orientation.Orientation;
+import tw.wn.roverstatus.RoverStatus;
+
+import static org.junit.Assert.assertEquals;
+
+public class RoverTest {
+
+    private Rover rover;
+
+    @Ignore
+    @Test
+    public void runCommandAndChangeStatusFor12N() {
+        rover = new Rover(new RoverStatus(1, 2, Orientation.N));
+        rover.run("LMLMLMLMM");
+        assertEquals(new Rover(new RoverStatus(1, 5, Orientation.N)), rover);
+    }
+
+    @Ignore
+    @Test
+    public void runCommandAndChangeStatusFor33E() {
+        System.out.println("it runs----------------------");
+        rover = new Rover(new RoverStatus(3, 3, Orientation.E));
+        rover.run("MMRMMRMRRM");
+        assertEquals(new Rover(new RoverStatus(5, 5, Orientation.E)), rover);
+    }
+}
